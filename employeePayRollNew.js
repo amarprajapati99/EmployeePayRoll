9587b9f2493c9a295d92c93b7a7b3df9575c9e5d
Prahlad Prajapati <58871235+amarprajapati99@users.noreply.github.com>
2021-05-15 11:50:18 +0530

    Added : On submit populate employee payroll data

---
+++ employeePayRollNew.js
@@ -0,0 +1,25 @@
+let employeePayroll = require("./employeePayroll.js");
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  const name = document.querySelector("#name");
+  const nameError = document.querySelector(".name-error");
+  name.addEventListener("input", function () {
+    if (name.value.length == 0) {
+      nameError.textContent = "";
+      return;
+    }
+    try {
+      new employeePayrollApp().name = name.value;
+      nameError.textContent = "";
+    } catch (e) {
+      nameError.textContent = e;
+    }
+
+    const salary = document.querySelector("#salary");
+    const output = document.querySelector(".salary-output");
+    output.textContent = salary.value;
+    salary.addEventListener("input", function () {
+      output.textContent = salary.value;
+    });
+  });
+});
